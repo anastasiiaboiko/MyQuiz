588c671d9bf1bc648dd1b4e32cf93f003729cad1
anastasiiaboiko <anastacia.boiko@gmail.com>
2014-11-19 22:06:13 +0200

    Project

---
+++ MyQuiz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyQuiz
+//
+//  Created by Anastasia Boiko on 11/19/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyQuiz/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MyQuiz
+//
+//  Created by Anastasia Boiko on 11/19/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
